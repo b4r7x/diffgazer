3430aad9e326a419e8f8a47693477b660116cb77
b4r7x <b4r7dev@gmail.com>
2026-03-23 06:53:39 +0100

    cleanup docs and fix keyscope examples

--- apps/docs/src/lib/component-list-data.ts
+++
@@ -1,7 +0,0 @@
-import { componentLists } from "@/generated/library-data"
-
-type ComponentListEntry = { name: string; title: string; description: string }
-
-export function getComponentList(libraryId: string): ComponentListEntry[] {
-  return (componentLists[libraryId] ?? []) as ComponentListEntry[]
-}
